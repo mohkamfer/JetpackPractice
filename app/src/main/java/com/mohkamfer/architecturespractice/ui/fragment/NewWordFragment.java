1c0643d8a6df26f267d9bf8bd127ad840562a0ad
Muhammad Kamal <mohammedkamalfergany@yahoo.com>
2020-02-21 21:50:43 +0200

    Setup Navigation
    Signed-off-by: Muhammad Kamal <mohammedkamalfergany@yahoo.com>

---
+++ app/src/main/java/com/mohkamfer/architecturespractice/ui/fragment/NewWordFragment.java
@@ -0,0 +1,28 @@
+package com.mohkamfer.architecturespractice.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mohkamfer.architecturespractice.R;
+
+
+public class NewWordFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_word, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/mohkamfer/architecturespractice/ui/fragment/WordListFragment.java
@@ -0,0 +1,32 @@
+package com.mohkamfer.architecturespractice.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mohkamfer.architecturespractice.R;
+
+
+public class WordListFragment extends Fragment {
+
+    public WordListFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_word_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
